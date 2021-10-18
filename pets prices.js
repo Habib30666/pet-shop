80f2e50aea0647ff93c441a2c88f136193264b7a
Habib30666 <91126413+Habib30666@users.noreply.github.com>
2021-10-18 13:22:08 +0100

    styles update

---
+++ pets prices.js
@@ -0,0 +1,31 @@
+var products = {
+    123: {
+    
+        name : "Dogs",
+        desc : "",
+        img : "",
+        price : 120
+    
+     
+    
+    },
+    
+    123: {
+    
+        name : "Cats",
+        desc : "",
+        img : "",
+        price : 110
+    
+    
+    },
+    
+    123: {
+    
+        name : "Rabbits",
+        desc : "",
+        img : "",
+        price : 50
+    
+    }
+    
